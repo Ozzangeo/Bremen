1d15953fb418e5a39a433a6cabfc20822a59b892
Ozzangeo <Ozzangeo0523@gmail.com>
2025-02-25 05:53:53 +0900

    .

---
+++ Assets/Scripts/Ozi/ChartPlayer/BremenChartObject.cs
@@ -0,0 +1,12 @@
+﻿using Ozi.ChartEditor;
+using UnityEngine;
+
+namespace Ozi.ChartPlayer {
+    [CreateAssetMenu(fileName = "BremenChart", menuName = "Bremen/ChartObject")]
+    public class BremenChartObject : ScriptableObject {
+        [field: SerializeField] public TextAsset BremenChartText { get; private set; }
+        [field: SerializeField] public AudioClip Clip { get; private set; }
+
+        public BremenChart Chart => JsonUtility.FromJson<BremenChart>(BremenChartText.text);
+    }
+}
