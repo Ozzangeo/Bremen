5f720ec0acf3569f98831f60b6fb45abf776e6d1
Ozzangeo <Ozzangeo0523@gmail.com>
2025-02-25 15:14:31 +0900

    Weapon Visualize

--- Assets/Scripts/Ozi/Weapon/Utility/ScreenBlinder.cs
+++ Assets/Scripts/Ozi/Weapon/Utility/ScreenBlinder.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 namespace Ozi.Weapon.Utility {
     public class ScreenBlinder : MonoBehaviour {
+        public const float SHOW_TIME = 1.0f;
+
         private static ScreenBlinder Instance {
             get {
                 if (!_isAllocated
@@ -23,7 +29,8 @@ namespace Ozi.Weapon.Utility {
         private static ScreenBlinder _instance;
         private static bool _isAllocated = false;
 
-        [SerializeField] private Image _image;
+        [SerializeField] private Volume _volume;
+        private IEnumerator _show;
 
         private void Awake() {
             if (_isAllocated) {
@@ -38,10 +45,36 @@ namespace Ozi.Weapon.Utility {
         }
 
         public static void Show(float alpha) {
-            var color = Instance._image.color;
-            color.a = alpha;
+            var instance = Instance;
 
-            Instance._image.color = color;
+            if (instance._show is not null) {
+                instance.StopCoroutine(instance._show);
+            }
+
+            var profile = instance._volume.profile;
+            if (!profile.TryGet(out Vignette vignette)) {
+                return;
+            }
+
+            instance._show = Show(vignette.intensity.value, alpha, SHOW_TIME, o => vignette.intensity.value = o);
+            instance.StartCoroutine(instance._show);
+        }
+
+        private static IEnumerator Show(float start, float end, float time, Action<float> setter) {
+            float cur_time = 0.0f;
+
+            while (cur_time < time) {
+                float t = cur_time / time;
+
+                var lerp = Mathf.Lerp(start, end, t);
+                setter?.Invoke(lerp);
+
+                cur_time += Time.deltaTime;
+
+                yield return null;
+            }
+
+            setter?.Invoke(end);
         }
     }
 }
